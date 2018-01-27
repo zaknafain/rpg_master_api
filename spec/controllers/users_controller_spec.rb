65288d933298b8f400b797402224ee3012b68d4b
David Seeherr <david.seeherr@gmail.com>
2018-01-27 20:56:19 +0100

    Added some user controller tests

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe UsersController do
-  
+
   let(:user) { FactoryBot.create(:user, email: "foo@bar.com", password: "secret pee", password_confirmation: "secret pee") }
   let(:authenticated_header) do
     token = Knock::AuthToken.new(payload: { sub: user.id, name: user.name }).token
@@ -16,7 +16,7 @@ RSpec.describe UsersController do
     it 'returns all users as json' do
       request.headers.merge! authenticated_header
       get :index
-  
+
       expect(response.status).to eq(200)
       expect(response.body).to include(user.name)
     end
@@ -25,6 +25,22 @@ RSpec.describe UsersController do
       get :index
 
       expect(response.status).to eq(401)
-    end 
+    end
+  end
+
+  describe "GET show" do
+    it "returns the user as json" do
+      request.headers.merge! authenticated_header
+      get :show, id: user.id
+
+      expect(response.status).to eq(200)
+      expect(response.body).to include(user.name)
+    end
+
+    it 'needs authentication' do
+      get :show, id: user.id
+
+      expect(response.status).to eq(401)
+    end
   end
 end

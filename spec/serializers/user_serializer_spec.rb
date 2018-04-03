015eb1f8e6a1e4a023e606c7323326549cb21c46
David Seeherr <david.seeherr@gmail.com>
2018-04-03 09:40:37 +0200

    Fixed user serializer spec

--- spec/serializers/user_serializer_spec.rb
+++ spec/serializers/user_serializer_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe UserSerializer do
 
   let(:user)            { FactoryBot.create(:user) }
-  let(:serialized_hash) { UserSerializer.new(user).serializable_hash }
+  let(:serialized_hash) { UserSerializer.new(user, scope: user, scope_name: :current_user).serializable_hash }
 
   it "serializes the user" do
     expect(serialized_hash[:id]).to eq(user.id)

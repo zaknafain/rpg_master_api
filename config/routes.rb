232a07f76123d24fbc832c12a2598afdddca5af7
David Seeherr <david.seeherr@gmail.com>
2019-01-21 17:16:42 +0100

    Lint the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-
   post 'user_token' => 'user_token#create'
 
-  resources :users, only: [:index, :show, :update, :destroy] do
+  resources :users, only: %i[index show update destroy] do
     get :me, on: :collection
   end
 end
